9432b5b7b1f853eaf1e902437f1a72b0ffaea499
Mikle-I <ivanovm2020@icloud.com>
2022-09-17 21:56:20 +0300

    Слайдер для пк версии

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+//функция которая скрывает меню
 const menu = document.querySelector('.main-nav'),
       menuitem = document.querySelector('.main-nav__ul--mob_tab'),
       button = document.querySelector('.intro-nav__button'),
@@ -11,7 +12,9 @@ function visiblMenu(){
    
 };
 visiblMenu();
+// конец функции
 
+// обработчки событий нажатия кнопок меню/закрытие меню
 button.addEventListener("click", function() {
    menuitem.classList.toggle('menu--notvisible'), 
    menu.classList.toggle('menu--notvisible');
@@ -21,3 +24,69 @@ closemenu.addEventListener("click", function() {
    menuitem.classList.toggle('menu--notvisible'), 
    menu.classList.toggle('menu--notvisible');
 });
+
+// конец обработчика
+
+// слайдер вариант карусель
+
+  // Slider( карусель вариант)
+
+  const slids = document.querySelectorAll('.review__1'),
+  prev = document.querySelector('.review__buttonback'),
+  next = document.querySelector('.review__buttonup'),
+  total = document.querySelector('#total'),
+  current = document.querySelector('#current'),
+  slidesWrapper =document.querySelector('.review-wraper'),
+  slidesField = document.querySelector('.review__inner'),
+  width = window.getComputedStyle(slidesWrapper).width;
+
+  let slideIndex = 1;
+  let otstup = 0;
+
+
+  slidesField.style.width = 100 * slids.length + '%';
+  slidesField.style.display = 'flex';
+  slidesField.style.transition = '0.8s all';
+  slidesWrapper.style.overflow = 'hidden';
+  
+  
+  slids.forEach(slide =>{
+     slide.style.width = width;
+  });
+
+  next.addEventListener('click', () =>{
+      if (otstup == +width.slice(0, width.length - 2) * (slids.length - 1)){
+      otstup = 0;
+     } else {
+         otstup += +width.slice(0, width.length - 2)
+     }
+
+         slidesField.style.transform = `translateX(-${otstup}px)`;
+
+         if (slideIndex == slids.length){
+             slideIndex = 1;
+         } else {
+             slideIndex++;
+         }
+         
+     });
+  prev.addEventListener('click', () =>{
+     if (otstup == 0){
+      otstup = +width.slice(0, width.length - 2) * (slids.length - 1);
+    } else {
+        otstup -= +width.slice(0, width.length - 2)
+    }
+     
+    
+    if (slideIndex == 1){
+     slideIndex = slids.length;
+     } else {
+     slideIndex--;
+      }
+
+        
+        slidesField.style.transform = `translateX(-${otstup}px)`;
+ });
+
+// конец слайдера
+
